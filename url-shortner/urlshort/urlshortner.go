f7d59a40bd2ebb5bc555a2b6480f88040a5006a8
Gabriel Thomé <gabrielallan.thome@gmail.com>
2019-11-02 03:19:20 -0300

    add yaml handler

--- url-shortner/urlshort/urlshortner.go
+++ url-shortner/urlshort/urlshortner.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +18,36 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yml)
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := buildMap(pathUrls)
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+
+	err := yaml.Unmarshal([]byte(data), &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+
+	return pathUrls, nil
 }
